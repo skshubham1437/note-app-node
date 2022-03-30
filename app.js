9ef2e31aca6812b60acfa488bd3a33ea9aaf0240
skshubham1437 <skshubham1437@gmail.com>
2022-03-30 13:08:43 +0530

    git

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-// console.log("Starting App js");
-// import notes from './notes.js';
-// const notes = require('./notes.js');
-// console.log(notes.add(3,22));
-
-// const _ = require('lodash');
-// console.log(_.uniq([2, 2, 3, 4, 2, 3,]));
-
-// fs module
-
-// const fs = require('fs');
-// fs.writeFileSync('notes.txt', 'My name is Shubham');
-// fs.appendFileSync('notes.txt', ' \nI am a backend developer.');
-  
-// Validator module
- 
-// const validator = require('validator');
-// // import validator from 'validator';
-// console.log(validator.isEmail('skshubham1437@gmail.com'));
-
-// Chalk module
-
 const chalk = require('chalk');
 const { demandOption } = require('yargs');
-// console.log(chalk.green.bold('Success!'));
 
-// nodemon package - To automatically run node app
-// nodemon app.js
-
-// process.argv - To get command line arguments
-// console.log(process.argv);
-
-// yargs  -  NPM package to parse command line arguments
-const yargs = require('yargs');
-  // console.log(yargs.argv);
+const yargs = require('yargs'); // yargs  -  NPM package to parse command line arguments
   // customise yargs version
-  // yargs.version('1.2.0')
+  yargs.version('1.1.0')
   //add, remove, read, list
 
   const notes = require('./notes.js')
@@ -57,6 +26,7 @@ const yargs = require('yargs');
     handler(argv){
       // console.log('Title:' + argv.title + "\nDescription:" + argv.bodymea);
       notes.addNote(argv.title, argv.body)
+      console.log("Note Added")
     }
   })
 
@@ -73,6 +43,7 @@ const yargs = require('yargs');
       },  
     handler(argv){
       notes.removeNote(argv.title)
+      console.log("Note Removed")
     }
   })
   // Create list command
